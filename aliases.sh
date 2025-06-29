a0b1f3c30aa2d25410049ce7f78c9c292132b978
ReubenReny03 <reubenreny@karunya.edu.in>
2025-06-30 00:56:20 +0530

    Initial commit

---
+++ aliases.sh
@@ -0,0 +1,5 @@
+# Custom aliases live here – managed by alias‑manager.sh
+alias new_alias="echo Hello from new_alias"
+alias whatsapp="firefox --new-window web.whatsapp.com"
+alias remo_music="~/Documents/CLI_MUSIC/run.sh"
+alias hello="echo testing"
